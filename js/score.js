8e3fde0515ef94e52988213e8e2a35130ce20ac8
lucasestoes <103345730+lucasestoes@users.noreply.github.com>
2024-10-19 20:10:53 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 300) {
+    if (rank > 100) {
         return 0;
     }
-    if (rank > 20 && percent < 100) {
+    if (rank > 25 && percent < 100) {
         return 0;
     }
 
