fa89be89ed6da04b231c514b701009c5a1b1057c
lucasestoes <103345730+lucasestoes@users.noreply.github.com>
2026-01-03 13:49:14 -0600

    Adjust rank threshold in score function

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 2;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 150) {
+    if (rank > 140) {
         return 0;
     }
     if (rank > 50 && percent < 100) {
