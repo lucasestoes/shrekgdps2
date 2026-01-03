2c0d680b51457081c31d6e28350199558287f10c
lucasestoes <103345730+lucasestoes@users.noreply.github.com>
2026-01-03 13:18:27 -0600

    Update qualification percentage condition for records

--- js/pages/List.js
+++ js/pages/List.js
@@ -67,7 +67,7 @@ export default {
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 50"><strong>{{ level.percentToQualify }}%</strong> required (Main List)</p>
-                    <p v-else-if="selected +1 <= 150"><strong>100%</strong> required (Extended List)</p>
+                    <p v-else-if="selected +1 <= 100"><strong>100%</strong> required (Extended List)</p>
                     <p v-else>This level does not accept new records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
